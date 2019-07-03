137c0bff3b0e0a2996d8fc03406e1751f9663c82
Pawel Podhajski <pawel.podhajski@epfl.ch>
2019-07-03 21:09:00 +0200

    Bump OptiX to 5.1.1

--- var/spack/repos/builtin/packages/optix/package.py
+++ var/spack/repos/builtin/packages/optix/package.py
@@ -8,11 +8,11 @@ from spack import *
 class Optix(Package):
     """Nvidia OptiX is a ray tracing API."""
     homepage = "https://developer.nvidia.com/optix"
-    version('5.0.1','a1be032f246654ec4e4978b8c30e7774', extension='sh', expand=False)
-    url = "file:///gpfs/bbp.cscs.ch/project/proj3/development/deployment/optix.sh"
+    version('5.1.1','6e0671d49d08289c5c1ceceb9c505bbf', extension='sh', expand=False)
+    url = "file:///gpfs/bbp.cscs.ch/project/proj3/development/deployment/NVIDIA-OptiX-SDK-5.1.1-linux64-25109142.sh"
     phases = [ 'install']
 
     def install(self, spec, prefix):
-        set_executable('./optix.sh')
-        install = Executable('./optix.sh')
+        set_executable('./NVIDIA-OptiX-SDK-5.1.1-linux64-25109142.sh')
+        install = Executable('./NVIDIA-OptiX-SDK-5.1.1-linux64-25109142.sh')
         install('--skip-license', '--prefix=%s' % prefix)
